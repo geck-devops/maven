89f8aa3852df643bf35035e5207b9aa7f04cb514
geck-devops <cse5thsemgeck@gmail.com>
2025-04-11 00:43:44 +0530

    Create HelloWorldTest.java

---
+++ src/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
